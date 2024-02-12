6fa57c4689c603545d4f9e5b6cbd97080d756a5f
raomilind04 <72732775+raomilind04@users.noreply.github.com>
2024-02-12 21:17:20 +0530

    chunking working script

--- assignment_1/pi_service/chunking-script.py
+++ assignment_1/pi_service/chunking-script.py
@@ -1,31 +1,34 @@
 import os
-from io import open
 
+def split_file(input_file_path, output_file_prefix, num_chunks=8):
+    output_directory = "data"
+    if not os.path.exists(output_directory):
+        os.makedirs(output_directory)
 
-
-def split_file_row_wise(input_file_path, output_file_prefix, num_chunks=8):
-
-    row_count = 0
-    output_chunks = [[] for _ in range(num_chunks)] 
-    os.mkdir("data")
     with open(input_file_path, 'r') as input_file:
-        for line in input_file:
-            row_count += 1
-            chunk_index = min(range(num_chunks), key=lambda i: len(output_chunks[i]))
-            output_chunks[chunk_index].append(line)
+        headers = [next(input_file) for _ in range(2)]
+        lines = input_file.readlines()
+    
+    total_data_rows = len(lines)
+    rows_per_chunk = total_data_rows // num_chunks
+    extra_rows = total_data_rows % num_chunks 
 
-    for i, chunk_data in enumerate(output_chunks):
-        output_filename = f"data/{output_file_prefix}{i}.txt"
+    start_index = 0 
+    for i in range(num_chunks):
+        chunk_size = rows_per_chunk + (1 if i < extra_rows else 0)
+        end_index = start_index + chunk_size
+
+        chunk_lines = lines[start_index:end_index]
+        start_index = end_index
+
+        output_filename = f"{output_directory}/{output_file_prefix}{i}.txt"
         with open(output_filename, 'w') as output_file:
-            output_file.writelines(chunk_data)
+            output_file.writelines(headers + chunk_lines)
 
-    print(f"split into {num_chunks} chunks.")
-
+    print(f"Split into {num_chunks} chunks")
 
 input_file_path = "master_file.txt"
 output_file_prefix = "chunk_"
 num_chunks = 4
 
-split_file_row_wise(input_file_path, output_file_prefix, num_chunks)
-
-
+split_file(input_file_path, output_file_prefix, num_chunks)
