f251e1c4b3db0f6dc929ae3a6a820bcafe5a3f10
raomilind04 <72732775+raomilind04@users.noreply.github.com>
2024-03-26 16:10:27 +0530

    add text box for starting node

--- Distributed-HashTable/Server/server.py
+++ Distributed-HashTable/Server/server.py
@@ -12,23 +12,23 @@ logger = logging.getLogger(__name__)
 app = Flask(__name__)
 CORS(app)
 
-@app.route('/lookup/<int:key>', methods=['GET'])
-def lookup_api(key):
+@app.route('/lookup/<int:key>/<int:starting_node>', methods=['GET'])
+def lookup_api(key, starting_node): 
     try:
-        starting_node = int(os.environ['STARTING_NODE_ID'])
-        starting_service_name = f"chord-node-service-{starting_node}"
+        starting_service_name = f"chord-node-service-{starting_node}" 
         starting_address = f"http://{starting_service_name}.default.svc.cluster.local:8080/lookup/{key}"
         logger.info(f"starting_address: {starting_address}")
 
         response = requests.get(starting_address, allow_redirects=True)
-            
+
         if response.status_code == 200:
             return response.json()
         else:
-            logger.error(e)
+            logger.error(f"Failed to get response from {starting_address}")
             return jsonify({'error': f"Failed to get response from {starting_address}"}), 500
+
     except Exception as e:
-        logger.err(e)
+        logger.error(e)
         return jsonify({'error': str(e)}), 400
     
 @app.route('/')
